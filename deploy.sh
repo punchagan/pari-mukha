6cd0ba80377781bd71c934cd6c0a1450d6298ca3
Puneeth Chaganti <punchagan@muse-amuse.in>
2016-10-08 20:08:14 +0530

    Add deployment of about page

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 set -e
 GIT_URL=$(git remote get-url origin)
+ABOUT=resources/public/about.html
+
+# Create abou page
+git checkout -- "${ABOUT}"
+markdown -o html5 README.md >> "${ABOUT}"
+echo "</body>\n</html>" >> "${ABOUT}"
+
+# Build cljs
 lein clean
 lein cljsbuild once min
+
+# Push to GitHub
 pushd resources/public
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "${GIT_URL}" master:gh-pages
 popd
+
+# Clean up
 rm -fr resources/public/.git
+git checkout -- "${ABOUT}"
