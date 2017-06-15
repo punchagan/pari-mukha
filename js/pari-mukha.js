d3d8e1e37a984bbf927f57637465ee44af94d6b8
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-06-15 10:34:20 +0530

    Show India Map

---
+++ js/pari-mukha.js
@@ -0,0 +1,17 @@
+var india_coords = [22, 81],
+    zoom_level = 5,
+    max_zoom = 16,
+    tile_url = "//server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+    pari_attribution = "Photos &copy; <a href=https://ruralindiaonline.org>People's Archive of Rural India</a>",
+    map_attribution = "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ",
+    india_bounds = [[37, 67] [0, 98]];
+
+
+var map = L.map('map-container').setView(india_coords, zoom_level);
+L.tileLayer(tile_url, {
+    maxZoom: max_zoom,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        map_attribution + ', ' +
+        pari_attribution,
+    id: 'mapbox.streets'
+}).addTo(map);
