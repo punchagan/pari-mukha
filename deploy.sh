c66974210dadd9f838fe5dfb122bd7a16aefe6e6
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-06-19 23:30:11 +0530

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+GIT_URL=$(git remote get-url origin)
+ABOUT=about.html
+
+# Create abou page
+git checkout -- "${ABOUT}"
+markdown -o html5 README.md >> "${ABOUT}"
+echo "</body></html>" >> "${ABOUT}"
+
+# Push to GitHub
+mkdir -p resources/data/
+cp -a *.html css js resources/
+cp data/faces.json resources/data/
+pushd resources/
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "${GIT_URL}" master:gh-pages
+popd
+
+# Clean up
+rm -fr resources/
+git checkout -- "${ABOUT}"
