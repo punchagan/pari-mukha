a88dc6aa7290534e90e96859e091b8a309f24630
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-06-27 23:42:09 +0530

    Use bootstrap for the pages

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,16 @@
 set -e
 GIT_URL=$(git remote get-url origin)
 ABOUT=about.html
+PARTIAL=about-partial.html
 
-# Create abou page
+# Create about page
 git checkout -- "${ABOUT}"
-markdown -o html5 README.md >> "${ABOUT}"
-echo "</section></body></html>" >> "${ABOUT}"
+markdown -o html5 README.md >> "${PARTIAL}"
+sed -i '/XXX/{
+    s/XXX//g
+    r about-partial.html
+}' about.html
+rm about-partial.html
 
 # Push to GitHub
 mkdir -p resources/data/
